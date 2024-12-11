5ccbc5dcc97b4ed347ec1e6fe32e2ceddeeb9740
vectordxy <jnoncode@gmail.com>
2024-12-11 20:03:46 +0900

    feat: add types page

---
+++ app/docs/set-up/types/page.tsx
@@ -0,0 +1,15 @@
+import Article from "@/src/components/docs/Article";
+import CodeBlock from "@/src/components/docs/CodeBlock";
+
+export default function page() {
+  return (
+    <Article title="Types">
+      If your project is built with typescript, you can add types. StayedCSS
+      provide 2 types, MainInput and Config.
+      <CodeBlock language="jsx" code={example} />
+    </Article>
+  );
+}
+
+const example = `import { MainInput, Config } from "stayedcss/client";
+`;
